70a2cabdf4275a4b20eb18d82ab3dab3d52dfd3d
am-gooru <ashish@gooru.org>
2017-01-17 18:08:50 +0530

    Remove the activeJDBC dependency and work off plain jdbc

--- src/main/java/org/gooru/nucleus/libs/tenant/db/services/TenantStoreService.java
+++ src/main/java/org/gooru/nucleus/libs/tenant/db/services/TenantStoreService.java
@@ -1,14 +1,15 @@
 package org.gooru.nucleus.libs.tenant.db.services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.sql.DataSource;
 
 import org.gooru.nucleus.libs.tenant.db.entities.Tenant;
-import org.javalite.activejdbc.Base;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,25 +25,35 @@ public final class TenantStoreService {
     }
 
     public static Map<String, Tenant> fetchAllActiveTenants(DataSource dataSource) {
-        List<Tenant> tenants;
         Map<String, Tenant> store = new HashMap<>(20);
+        Connection connection = null;
+        ResultSet resultSet = null;
+        PreparedStatement statement = null;
         try {
-            setupTransaction(dataSource);
-            tenants = Tenant.findBySQL(Tenant.QUERY_FETCH_TENANT);
-            tenants.forEach(tenant -> store.put(tenant.getTenantId(), tenant));
-            Base.commitTransaction();
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement(Tenant.QUERY_FETCH_TENANT);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Tenant tenant = new Tenant(resultSet);
+                store.put(tenant.getTenantId(), tenant);
+            }
+            LOGGER.info("Initialized tenant store with '{}' records", store.size());
         } catch (Exception e) {
             LOGGER.error("Not able to initialize tenant store", e);
-            Base.rollbackTransaction();
         } finally {
-            Base.close();
-        }
-        return store;
-    }
-
-    private static void setupTransaction(DataSource dataSource) throws SQLException {
-        Base.open(dataSource);
-        Base.connection().setReadOnly(true);
-        Base.openTransaction();
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } return store;
     }
 }
